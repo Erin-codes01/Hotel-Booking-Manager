286265398b2d2502404bbe8a62e76a230f84da8d
Erin Shea <e.l.shea23@gmail.com>
2026-03-08 16:55:47 -0500

    Shells Added

---
+++ Booking.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace HotelBookingManager
+{
+    public sealed class Booking
+    {
+        
+		//Public Variables Required by the assignment
+		public string RoomNumber { get; }
+        public string GuestName { get; }
+        public DateTime CheckIn { get; private set; }
+        public DateTime CheckOut { get; private set; }
+
+        public Booking(string roomNumber, string guestName, DateTime checkIn, DateTime checkOut)
+        {
+            //Check if the roomNumber or guestName are null or whitespace and throw an exception that they are required
+			//Check if checkOut is before checkIn with <=. Throw an exception if true
+			
+			
+
+			//If they validate, assign the instance variables here:
+            
+        }
+
+        public void Reschedule(DateTime newIn, DateTime newOut)
+        {
+            //Check if the newOut DateTime is before newIn. Use <= and throw an exception if true
+			//If not, set the Check In and Check Out times to the new ones
+			
+			
+        }
+
+		//Create an override for ToString
+		//Print according to assignment guidelines: [{RoomNumber}] {CheckIn:MM/dd HH:mm}–{CheckOut:MM/dd HH:mm}  {GuestName}
+
+    }
+}
